13667703f9b9422c98fb0f104308cd4a39a0753a
Chris Parker <chris.parker2@gmail.com>
2022-10-29 11:52:36 +0100

    Test success

--- .github/scripts/produce-jekyll-yml.py
+++ .github/scripts/produce-jekyll-yml.py
@@ -10,41 +10,5 @@ class Group:
 
 print("Reading theta.json")
 
-f = open("brook/theta.json", "r")
-fileContent = f.read()
-f.close()
-
-print("theta.json read. Parsing as json")
-
-theta = json.loads(fileContent)
-
-print("Parsed theta.json successfully. Generating groups.yml")
-
-groups = []
-
-for simpleGroup in theta["caboodle"]:
-  groupName = simpleGroup["name"]
-  batch = simpleGroup["batch"]
-  for isoGroup in simpleGroup["isoGroup"]:
-    for permRep in isoGroup["permReps"]:
-      permRepName = permRep["name"]
-      newGroup = Group(batch, groupName, permRepName)  
-      groups.append(newGroup)
-
-f = open("docs/_data/groups.yml", "w")
-for group in groups:
-  f.write("- groupName: ")
-  f.write(group.groupName)
-  f.write("\n")
-  
-  f.write("  batch: ")
-  f.write(group.batch)
-  f.write("\n")
-  
-  f.write("  permRepName: ")
-  f.write(group.permRepName)
-  f.write("\n")
-    
-f.close()
 
 print("Generated groups.yml successfully")
