7a48151315ed42dcc961e7012d989cceea4dabb4
Chris Parker <chris.parker2@gmail.com>
2022-11-07 23:41:34 +0000

    Write allreps and permreps

--- .github/scripts/produce-jekyll-files.py
+++ .github/scripts/produce-jekyll-files.py
@@ -28,3 +28,28 @@ print("Created _data folder. Writing theta.json to output.")
 with open('docs/_data/theta.json', 'w') as f:
     json.dump(theta, f)
 
+print("theta.json written. Creating allreps folder")
+
+os.mkdir("docs/_data/allreps")
+
+print("allreps folder created. Writing allreps")
+
+for simpleGroup in theta["caboodle"]:
+  with open('docs/_data/allreps/' + simpleGroup["name"] + '.json', 'w') as f:
+    json.dump(simpleGroup, f)      
+
+print("allreps written. Creating permreps folder")
+
+os.mkdir("docs/_data/permreps")
+
+print("permreps folder created. Writing permreps")
+
+for simpleGroup in theta["caboodle"]:
+  groupName = simpleGroup["name"]
+  batch = simpleGroup["batch"]
+  for isoGroup in simpleGroup.get("isoGroups", []):
+    for permRep in isoGroup.get("permReps", []):
+      with open('docs/_data/permreps/' + permRep["name"] + '.json', 'w') as f:
+        json.dump(permRep, f)
+        
+print("permreps written")
